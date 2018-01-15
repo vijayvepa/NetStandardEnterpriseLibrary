dff5b073d4df016798024b1c2f547d7c44f95628
Vijay Vepakomma <vijay.vepakomma@philips.com>
2018-01-15 11:00:58 -0500

    Semantic Logging EventSourceAnalyzer

--- Microsoft.Practices.EnterpriseLibrary.SemanticLogging/Properties/AssemblyInfo.cs
+++ Microsoft.Practices.EnterpriseLibrary.SemanticLogging/Properties/AssemblyInfo.cs
@@ -3,3 +3,4 @@
 [assembly: InternalsVisibleTo("Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Database")]
 [assembly: InternalsVisibleTo("Microsoft.Practices.EnterpriseLibrary.SemanticLogging.TextFile")]
 [assembly: InternalsVisibleTo("Microsoft.Practices.EnterpriseLibrary.SemanticLogging.EventSourceAnalyzer")]
+[assembly: InternalsVisibleTo("Microsoft.Practices.EnterpriseLibrary.SemanticLogging.Utility")]
